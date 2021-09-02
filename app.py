d23276df6a879f2354e50231725150cc8307cf68
apinnya <89177094+apinnya@users.noreply.github.com>
2021-09-02 10:48:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ data = [
 
 @app.route('/')
 def hello():
-    return "Hello Flask-Herok"
+    return "Hello อภิญญา พรมพิลาศ เลขที่ 27 ชั้น ม.4/6"
 
 @app.route('/api', methods=['GET'])
 def get_api():
